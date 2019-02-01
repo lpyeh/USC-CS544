398996e9b54ac25c4804ba6ac2591a7d584dabaa
lpyeh <lpyeh@umich.edu>
2019-01-31 19:33:11 -0800

    training file for naive bayes implementation

---
+++ nblearn.py
@@ -0,0 +1,21 @@
+#!usr/bin/python
+import sys
+import math
+import os
+
+
+"""
+Training file for Naive Bayes program for CSCI 544.
+
+Author: Leigh Yeh
+Date: 1/31/2019
+University of Southern California
+"""
+
+def load_data():
+    for directory in os.listdir(input_path):
+        for file_ in os.path.join(input_path, directory):
+
+
+if __name__=='__main__':
+    input_path = sys.argv[1]
